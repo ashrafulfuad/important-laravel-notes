8c5786f162e036b197d2fe3ec016b96231586777
Ashraful Fuad <48980302+ashrafulfuad@users.noreply.github.com>
2019-05-24 11:42:40 +0600

    Update Checking.php

--- Checking.php
+++ Checking.php
@@ -1,7 +1,14 @@
+function sliderinsert()
+    {
+        print_r($_POST);                    // checking all form data after submitting in PHP
+        echo $_POST['product_name'];      // checking form data after submitting in PHP specific array
+    }
+
+
 function sliderinsert(Request $request)
     {
-        print_r($_POST);                    // checking form data after submitting in PHP
-        echo $_POST['starting_price'];      // checking form data after submitting in PHP specific array
+      print_r($request->all());             // checking all form data after submitting in Laravel
+      echo $request->product_name;        // checking form data after submitting in Laravel specific array
     }
     
     
