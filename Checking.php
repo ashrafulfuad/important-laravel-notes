77e46e1536f5db0be615ce08423bdce39de151cb
Ashraful Fuad <48980302+ashrafulfuad@users.noreply.github.com>
2019-05-24 11:35:19 +0600

    Update Checking.php

--- Checking.php
+++ Checking.php
@@ -1,6 +1,7 @@
 function sliderinsert(Request $request)
     {
-      print_r($_POST);       // checking form data after submitting in PHP
+        print_r($_POST);                    // checking form data after submitting in PHP
+        echo $_POST['starting_price'];      // checking form data after submitting in PHP specific array
     }
     
     
