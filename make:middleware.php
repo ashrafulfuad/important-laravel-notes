086bc07121e30fa42c0ed32445330df692ef8bbd
Ashraful Fuad <48980302+ashrafulfuad@users.noreply.github.com>
2019-06-25 10:51:20 +0600

    Update make:middleware.php

--- make:middleware.php
+++ make:middleware.php
@@ -1,1 +1,20 @@
+php artisan make:middleware CheckRole                     // command it in your gitbash
 
+'checkrole' => \App\Http\Middleware\CheckRole::class,     // To register this middleware go to kernel.php add this line in 
+                                                          // protected $route Middleware = [.. here ...]
+
+public function handle($request, Closure $next)           
+    {
+
+        if (Auth::user()->role == 2) {                    // Go to CheckRole.php add this if condition inside the 6 to 14 lines code
+          return redirect('customer/dashboard');
+        }
+
+        return $next($request);
+    }
+
+
+
+                যদি login  করার পর এই লিংক এ যায় যে ('/home') তাহলে HomeController এ যাও এবং এড করো 
+                      $this -> middleware('checkrole');
+                __construct() নামক function এর ভিতর।                                   
